9c49a7277dfdcf037434c5a786d95a5cf620e386
412501-Wuthrich <412501@tecnicatura.frc.utn.edu.ar>
2025-08-15 11:08:32 -0300

    Seccion 5: Dragon Ball Page Finished

--- 02-bases/src/app/pages/dragonball-page/dragonball-page.component.ts
+++ 02-bases/src/app/pages/dragonball-page/dragonball-page.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
+
+interface Character {
+  id: number,
+  name: string,
+  power: number
+}
 
 @Component({
   selector: 'app-dragonball-page',
@@ -7,5 +13,20 @@ import { Component } from '@angular/core';
   styleUrl: './dragonball-page.component.css'
 })
 export class DragonballPageComponent {
+
+  characters = signal<Character[]>([
+    {
+      id:1, name:'Goku',power: 9001
+    },
+    {
+      id:2, name:'Piccoro',power: 4500
+    },
+    {
+      id:3, name:'Vegeta',power: 16000
+    },
+    {
+      id:4, name:'Yamtcha',power: 500
+    },
+  ])
 
 }
